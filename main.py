3208183b4e73d30e449c8d8f6bb3d944dec203fd
onybic <onybic@gmail.com>
2024-09-02 20:26:02 +0200

    asteroids, player, movement, basic shooting

--- main.py
+++ main.py
@@ -1,18 +1,56 @@
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    clock = pygame.time.Clock()
+
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable_group, drawable_group)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable_group, drawable_group)
+    AsteroidField.containers = updatable_group
+    asteroid_field = AsteroidField()
+
+    Shot.containers = (shots, updatable_group, drawable_group)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable_group:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
+
+        for obj in drawable_group:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # 60fps limit
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
